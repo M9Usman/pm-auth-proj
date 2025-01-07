e8b1d5385cece2b05d53e3f449529930ca59949f
Mo.Usman <musman38302@gmail.com>
2025-01-07 12:52:41 +0500

    `check/Error`

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { PrismaService } from "src/prisma.service";
 import { AuthController } from "./auth.controller";
 import { MailerModule } from "src/mailer/mailer.module";
 import { MailerService } from "src/mailer/mailer.service";
+import { AuthGuard } from "src/auth/guards/auth.guard";
 @Module({
     controllers:[AuthController],
-    providers:[AuthService,PrismaService,MailerService],
+    providers:[AuthService,PrismaService,MailerService,AuthGuard],
     imports:[
         MailerModule
     ],
